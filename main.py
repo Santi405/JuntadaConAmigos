ca3c6f14fdc8fad0eb90f09a43ed3f9e00e3f019
Santino Zanin <69124536+Santi405@users.noreply.github.com>
2023-11-13 01:53:41 -0300

    No pide costo total, menos comas

--- main.py
+++ main.py
@@ -1,88 +1,84 @@
-import os
-import sys
+import os 
+import sys 
+  
+def ingresoAmigos(): 
+     """ 
+     Ingresa los datos de los amigos y los almacena en una lista de diccionarios 
+     """                     
+     vacaTotal = 0 
+     listaAmigos = [] 
+  
+     nombre = input("Ingrese el nombre del amigo (enter para terminar): ")       # Ingresa nombre y cuanto puso 
+     while nombre != "": 
+         amigo = {} 
+         cuantoPuso = round(float(input(f"Ingrese cuanto puso {nombre}: ")), 2)
+         amigo["Nombre"] = nombre 
+         amigo["cuantoPuso"] = cuantoPuso 
+         listaAmigos.append(amigo) 
+         nombre = input("Ingrese el nombre del amigo (enter para terminar): ") 
+  
+     return listaAmigos 
+  
+def calculoCosto(listaAmigos): 
+     """ 
+     Calcula el costo promedio de la juntada y compara el costo total con la cantidad juntada por los amigos. 
+     """ 
+     clear() 
+     vacaTotal = 0 
 
-def ingresoAmigos():
-    """
-    Ingresa los datos de los amigos y los almacena en una lista de diccionarios
-    """                    
-    vacaTotal = 0
-    listaAmigos = []
-
-    nombre = input("Ingrese el nombre del amigo (* para terminar): ")       # Ingresa nombre y cuanto puso
-    while nombre != "*":
-        amigo = {}
-        cuantoPuso = float(input(f"Ingrese cuanto puso {nombre}: "))
-        amigo["Nombre"] = nombre
-        amigo["cuantoPuso"] = cuantoPuso
-        listaAmigos.append(amigo)
-        nombre = input("Ingrese el nombre del amigo (* para terminar): ")
-
-    return listaAmigos
-
-def calculoCosto(listaAmigos):
-    """
-    Calcula el costo promedio de la juntada y compara el costo total con la cantidad juntada por los amigos.
-    """
-    clear()
-    vacaTotal = 0
-    costoTotal = int(input("Ingrese el costo total de la juntada: "))       # Ingresa costo total
-
-    for amigo in listaAmigos:                                               # Suma cuanto puso cada uno
-        vacaTotal += amigo["cuantoPuso"]
-
-    if vacaTotal != costoTotal:
-        clear()
-        print("Error, los valores ingresados no son correctos. Ingrese los valores de nuevo.\n")
-        ingresoAmigos()
-
-    costoProm = costoTotal / len(listaAmigos)                               # Calcula el costo de cada uno
-    return costoProm
-
-def calculoDeudas(amigos, costoProm):
-    """
-    Calcula deudas y devuelve dos listas de diccionarios, una con los amigos que pusieron de menos (ratas) y una con los amigos que pusieron de más (prestamistas)
-    """
-    debePlata = []
-    leDebenPlata = []
-
-    for amigo in amigos:
-        if amigo["cuantoPuso"] < costoProm:
-            amigo["cuantoDebe"] = round(costoProm - amigo["cuantoPuso"], 2)
-            amigo.pop("cuantoPuso") 
-            debePlata.append(amigo)
-
-        elif amigo["cuantoPuso"] > costoProm:
-            amigo["cuantoLeDeben"] = round(amigo["cuantoPuso"] - costoProm, 2)
-            amigo.pop("cuantoPuso")
-            leDebenPlata.append(amigo)
-
-    return debePlata, leDebenPlata
-            
-def pagoDeDeudas(endeudados, prestamistas):
-    """
-    Indica quien le debe a quien y cuanto
-    """
-    for rata in endeudados:
-        for prestamista in prestamistas:
-            if rata["cuantoDebe"] >= prestamista["cuantoLeDeben"]:
-                print(f"El amigo {rata['Nombre']} le debe pagar a {prestamista['Nombre']} ${prestamista['cuantoLeDeben']}")
-                rata["cuantoDebe"] -= prestamista["cuantoLeDeben"]
-                prestamista["cuantoLeDeben"] = 0
-            else:
-                print(f"El amigo {rata['Nombre']} le debe pagar a {prestamista['Nombre']} ${rata['cuantoDebe']}")
-                prestamista["cuantoLeDeben"] -= rata["cuantoDebe"]
-                rata["cuantoDebe"] = 0
-                
-def clear():
-    if sys.platform == "linux":
-        os.system("clear")
-    elif sys.platfrom == "nt":
-        clear()
-
-
-listaAmigos = ingresoAmigos()
-costoPromedio = calculoCosto(listaAmigos)
-debe, leDeben = calculoDeudas(listaAmigos, costoPromedio)
-
+  
+     for amigo in listaAmigos:                                               # Suma cuanto puso cada uno 
+         vacaTotal += amigo["cuantoPuso"] 
+  
+  
+     costoProm = vacaTotal / len(listaAmigos)                               # Calcula el costo de cada uno 
+     return costoProm 
+  
+def calculoDeudas(amigos, costoProm): 
+     """ 
+     Calcula deudas y devuelve dos listas de diccionarios, una con los amigos que pusieron de menos (ratas) y una con los amigos que pusieron de más (prestamistas) 
+     """ 
+     debePlata = [] 
+     leDebenPlata = [] 
+  
+     for amigo in amigos: 
+         if amigo["cuantoPuso"] < costoProm: 
+             amigo["cuantoDebe"] = round(costoProm - amigo["cuantoPuso"], 2) 
+             amigo.pop("cuantoPuso")  
+             debePlata.append(amigo) 
+  
+         elif amigo["cuantoPuso"] > costoProm: 
+             amigo["cuantoLeDeben"] = round(amigo["cuantoPuso"] - costoProm, 2) 
+             amigo.pop("cuantoPuso") 
+             leDebenPlata.append(amigo) 
+  
+     return debePlata, leDebenPlata 
+  
+def pagoDeDeudas(endeudados, prestamistas): 
+     """ 
+     Indica quien le debe a quien y cuanto 
+     """ 
+     for rata in endeudados: 
+         for prestamista in prestamistas: 
+         	if rata["cuantoDebe"] != 0:
+	             if rata["cuantoDebe"] >= prestamista["cuantoLeDeben"]: 
+	                 print(f"El amigo {rata['Nombre']} le debe pagar a {prestamista['Nombre']} ${prestamista['cuantoLeDeben']}") 
+	                 rata["cuantoDebe"] -= prestamista["cuantoLeDeben"] 
+	                 prestamista["cuantoLeDeben"] = 0 
+	             else : 
+	                 print(f"El amigo {rata['Nombre']} le debe pagar a {prestamista['Nombre']} ${rata['cuantoDebe']}") 
+	                 prestamista["cuantoLeDeben"] -= rata["cuantoDebe"] 
+	                 rata["cuantoDebe"] = 0 
+  
+def clear(): 
+     if sys.platform == "linux": 
+         os.system("clear") 
+     elif sys.platfrom == "nt": 
+         clear() 
+  
+  
+listaAmigos = ingresoAmigos() 
+costoPromedio = calculoCosto(listaAmigos) 
+debe, leDeben = calculoDeudas(listaAmigos, costoPromedio) 
+  
 pagoDeDeudas(debe, leDeben)
-
